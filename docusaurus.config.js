a9a60ef8d93fbae8002c1ff767214398fb1651ba
Johannes Vedder <johannes.vedder@student.hpi.de>
2023-02-16 13:01:44 +0100

    fix favicon and margin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: '/img/favicon.ico',
 
   // GitHub Pages Deployment
   projectName: 'docs',
